5100a5b1a37f0dbeda3a7c197b075c2fa1fa1138
sanjeet131 <ysanjeet535@gmail.com>
2022-10-23 14:06:04 +0530

    added rich editor

--- app/src/main/java/com/ysanjeet535/androidrichtexteditorexploration/librarydemo/RichEditorOne.kt
+++ app/src/main/java/com/ysanjeet535/androidrichtexteditorexploration/librarydemo/RichEditorOne.kt
@@ -13,7 +13,84 @@ class RichEditorOne : AppCompatActivity() {
         binding = RichEditorOneBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+        setupEditor()
     }
 
+    private fun setupEditor() {
+        binding.editor.setEditorHeight(200)
+        binding.editor.setEditorFontSize(22)
 
+        binding.editor.setPadding(10, 10, 10, 10)
+        binding.editor.setPlaceholder("Insert text here...");
+
+        binding.editor.setOnTextChangeListener {
+            binding.preview.text = it
+        }
+
+        binding.actionUndo.setOnClickListener {
+            binding.editor.undo()
+        }
+
+        binding.actionRedo.setOnClickListener {
+            binding.editor.redo()
+        }
+
+        binding.actionBold.setOnClickListener {
+            binding.editor.setBold()
+        }
+
+        binding.actionItalic.setOnClickListener {
+            binding.editor.setItalic()
+        }
+
+        binding.actionSubscript.setOnClickListener {
+            binding.editor.setSubscript()
+        }
+
+        binding.actionSuperscript.setOnClickListener {
+            binding.editor.setSuperscript()
+        }
+
+        binding.actionStrikethrough.setOnClickListener {
+            binding.editor.setStrikeThrough()
+        }
+
+        binding.actionItalic.setOnClickListener {
+            binding.editor.setItalic()
+        }
+
+        binding.actionUnderline.setOnClickListener {
+            binding.editor.setUnderline()
+        }
+
+        binding.actionHeading1.setOnClickListener {
+            binding.editor.setHeading(1)
+        }
+
+        binding.actionHeading2.setOnClickListener {
+            binding.editor.setHeading(2)
+        }
+
+        binding.actionHeading3.setOnClickListener {
+            binding.editor.setHeading(3)
+        }
+
+        binding.actionHeading4.setOnClickListener {
+            binding.editor.setHeading(4)
+        }
+
+        binding.actionHeading5.setOnClickListener {
+            binding.editor.setHeading(5)
+        }
+
+        binding.actionHeading6.setOnClickListener {
+            binding.editor.setHeading(6)
+        }
+
+        binding.actionInsertCheckbox.setOnClickListener {
+            binding.editor.insertTodo()
+        }
+
+
+    }
 }
